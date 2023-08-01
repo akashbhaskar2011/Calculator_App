9bd01db21bcea0c3bcf025b969a380a9d9d5caf2
akashbhaskar2011 <63161311+akashbhaskar2011@users.noreply.github.com>
2023-08-02 00:49:16 +0530

    first commit just added the project to the directory

--- app/src/main/java/com/example/my_calculator/MainActivity.kt
+++ app/src/main/java/com/example/my_calculator/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.my_calculator.ui.theme.CalculatorViewModel
 import com.example.my_calculator.ui.theme.My_CalculatorTheme
 
@@ -21,7 +22,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             My_CalculatorTheme {
 
-              val viewModel=view
+              val viewModel= viewModel<CalculatorViewModel>()
 
                 val buttonSpacing=8.dp
             }
---
+++ app/src/main/java/com/example/my_calculator/ui/theme/Calculator.kt
@@ -0,0 +1,13 @@
+package com.example.my_calculator.ui.theme
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun Calculator(
+    state: CalculatorState,
+    modifier: Modifier=Modifier,
+
+){
+
+}
